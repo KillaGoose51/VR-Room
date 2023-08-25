0b13c43ff9ac0352049d921266b58147fdca8701
KillaGoose51 <93846546+KillaGoose51@users.noreply.github.com>
2023-08-25 15:18:45 -0500

    quicksave

---
+++ Assets/Scripts/player_movement.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public object Player { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
